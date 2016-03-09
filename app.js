b860f3b04594b627a7a3c7901a6d46ad5501ad10
Benjamim Sonntag <benjamimsonntag@gmail.com>
2016-03-09 14:42:31 +0000

    Updates styles.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'app'),
   dest: path.join(__dirname, 'public'),
   indentedSyntax: true,
-  sourceMap: true
+  sourceMap: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
